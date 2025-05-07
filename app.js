4814bbd121f21f3caae2246b4fb0d9a673ff9c7a
Carter Richmy <noelcarter216@gmail.com>
2025-05-07 11:50:44 -0600

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-// Function Declaration
-function greetUser(name) {
-  console.log("Hello, Noel!")
+let people = [18, 20, 16, 15, 21,];
+
+// Create a new empty array called adults
+let adults = [];
+
+for (let i = 0; i < people.length; i++) {
+    // Check if the person is an adult
+    if (people[i] >= 18) {
+        // Add the person to the adults array
+        adults.push(people[i]);
+    } // FALSE - Do nothing
+    // TRUE - Add element to the new array
 }
-// call the function
-greetUser("Noel")
